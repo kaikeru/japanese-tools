53a551db72cc863a4c4c705bb899c42721c39362
Kyle Wagner <kw@osano.com>
2022-08-29 16:08:30 -0600

    Adds script to load dictionaries

---
+++ scripts/load_dictionary.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/env bash
+
+set -e
+
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Download dictionaries
+
+dictionaryFilesDir="dictionary_files"
+
+cd $SCRIPT_DIR/..
+
+# Setup venv
+python3.9 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+# Run migrations
+python manage.py migrate
+
+# Fetch dictionaries
+
+rm -rf dictionary_files
+
+## Kanji
+wget --directory-prefix=$dictionaryFilesDir http://www.edrdg.org/kanjidic/kanjidic2.xml.gz
+gzip -d $dictionaryFilesDir/kanjidic2.xml.gz
+
+## JMDict
+wget --directory-prefix=$dictionaryFilesDir http://ftp.edrdg.org/pub/Nihongo/JMdict.gz
+gzip -d $dictionaryFilesDir/JMDict.gz
+mv $dictionaryFilesDir/JMDict $dictionaryFilesDir/JMDict.xml
+
+# Load dictonaries
+
+## Kanji
+python -m tools.load_kanji $dictionaryFilesDir/kanjidic2.xml
+
+## JMdict
+python -m tools.load_jmdict $dictionaryFilesDir/JMdict.xml
