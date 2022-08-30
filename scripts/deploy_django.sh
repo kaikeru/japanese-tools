e4d23ddf4f36b3329e162828ce0da6f885919a85
Kyle Wagner <kyle@kaikeru.com>
2022-08-29 20:36:27 -0600

    No input for collectstatic

--- scripts/deploy_django.sh
+++ scripts/deploy_django.sh
@@ -11,6 +11,6 @@ python3.9 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 python manage.py migrate
-python manage.py collectstatic -c
+python manage.py collectstatic -c --no-input
 deactivate
 sudo systemctl start gunicorn
