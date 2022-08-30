74ecbb4a481d15eb7b3e84ff844dfb751835b1d5
Kyle Wagner <kyle@kaikeru.com>
2022-08-29 20:14:07 -0600

    Deployment Script and Litestream
    * Adds deployment script
    * Installs and enables Litestream for backups

--- devops/scripts/setup_ubuntu.sh
+++ devops/scripts/setup_ubuntu.sh
@@ -11,6 +11,12 @@ apt install python3.9
 apt install sqlite3
 apt install nginx
 
+litestream_version="0.3.9"
+wget https://github.com/benbjohnson/litestream/releases/download/v$litestream_version/litestream-v$litestream_version-linux-amd64.deb
+dpkg -i litestream-v$litestream_version-linux-amd64.deb
+rm litestream-v$litestream_version-linux-amd64.deb
+
+
 cd $SCRIPT_DIR/../../
 
 python3.9 -m venv venv
@@ -26,10 +32,15 @@ ln -s /etc/nginx/sites-avilable/japanese-tools /etc/nginx/sites-enabled/
 
 cp ./etc/systemd/systems/gunicorn.service /etc/systemd/systems/gunicorn.service
 
+cp ./etc/litestream.yml /etc/litestream.yml
+
 # Start services
 
-systemctl start gunicorn
 systemctl enable gunicorn
+systemctl start gunicorn
 
-systemctl start nginx
 systemctl enable nginx
+systemctl start nginx
+
+systemctl enable litestream
+systemctl start litestream
---
+++ scripts/deploy_django.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd ..
+
+git pull
+sudo systemctl stop gunicorn
+rm -rf venv
+python3.9 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py collectstatic -c
+deactivate
+sudo systemctl start gunicorn
